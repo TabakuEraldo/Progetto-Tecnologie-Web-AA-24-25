006f559d35713779f9e7a1440055664c7a4971f0
patrick.sbrighi <patrick.sbrighi00@gmail.com>
2025-02-01 18:52:11 +0100

    inizio shop organizzazione cose

---
+++ DB/database.php
@@ -0,0 +1,20 @@
+<?php
+class DataBase{
+    private $db;
+
+    public function __construct($servername, $username, $password, $dbname){
+        $this->db = new mysqli($servername, $username, $password, $dbname);
+        if ($this->db->connect_error) {
+            die("Connection failed");
+        }        
+    }
+
+    public function getRandomProduct($number){
+        $query = $this->db->prepare("SELECT immagine, prezzo, disponibilita FROM prodotto ORDER BY RAND() LIMIT ?");
+        $query->bind_param('i',$number);
+        $query->execute();
+        $result = $query->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+}
+?>
