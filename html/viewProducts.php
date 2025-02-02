36d7ca1fe576ca7b48f9a2b40de61ae2f7a6a4f0
patrick.sbrighi <patrick.sbrighi00@gmail.com>
2025-02-02 01:36:39 +0100

    card gruop, prova bottoni quantità, 0 voglia di vivere, vado a letto

--- html/viewProducts.php
+++ html/viewProducts.php
@@ -1,12 +1,23 @@
-<h1>sistema il fatto che 3 vanno su una riga e poi va mobile first</h1>
-<?php foreach($pageParams["randProducts"] as $prod): ?>
-    <div class="card" style="width: 18rem;">
-    <img src=<?php echo "../img/".$prod["immagine"]?> class="card-img-top" alt="Immagine del prodotto">
-    <div class="card-body">
-        <h5 class="card-title"><?php echo $prod["nome"]?></h5>
-        <p class="card-text"><?php echo $prod["descrizione"]?></p>
-        <p class="card-text"><?php echo $prod["prezzo"]?></p>
-        <p class="card-text"><?php echo $prod["disponibilita"]?></p>
-    </div>
+<h1>le card sono troppo attaccate ai bordi e vanno rese più piccole</h1>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <?php foreach($pageParams["randProducts"] as $prod): ?>
+        <div class="col">
+            <div class="card h-100">
+                <img src=<?php echo "../img/".$prod["immagine"]?> class="card-img-top" alt="Immagine del prodotto">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $prod["nome"]?></h5>
+                    <p class="card-text"><?php echo $prod["descrizione"]?></p>
+                    <p class="card-text"><?php echo "<b>Prezzo: </b>".$prod["prezzo"]."$"?></p>
+                </div>
+                <footer class="card-footer">
+                    <div class="btn-group" role="group" aria-label="Basic outlined example">
+                        <button type="button" class="btn btn-outline-primary">-</button>
+                        <button type="button" class="btn btn-outline-primary">1</button>
+                        <button type="button" class="btn btn-outline-primary">+</button>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    <?php endforeach; ?>
 </div>
-<?php endforeach; ?>
+
