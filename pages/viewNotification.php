c23ee2f8ce0f36737a107c7fc2ea9576ffae722d
patrick.sbrighi <patrick.sbrighi00@gmail.com>
2025-02-04 17:48:58 +0100

    Visualizzazione notifiche

--- pages/viewNotification.php
+++ pages/viewNotification.php
@@ -1,1 +1,19 @@
-<h1>notifiche</h1>
+<h2 class="text-center mb-3">Le tue notifiche</h2>
+<table class="table">
+  <tbody>
+    <?php if (!empty($pageParams["notifiche"])): ?>
+        <?php foreach($pageParams["notifiche"] as $not): ?>
+            <tr>
+                <?php if($not["isLetto"] == 0):?>
+                    <th scope="row"><strong><?php echo $not["titolo"]?></strong></th>
+                <?php else: ?>
+                    <th scope="row"><?php echo $not["titolo"]?></th>
+                <?php endif; ?>
+                <td class="text-end"><a href=#>leggi</a></td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p class="text-muted text-center w-100">Nessuna notifica trovata</p>
+    <?php endif; ?>
+  </tbody>
+</table>
--- php/notification.php
+++ php/notification.php
@@ -1,6 +1,16 @@
 <?php
 require_once 'start.php';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$pageParams["notifiche"] = $db->getNotification($_SESSION['user_id'], $_SESSION['user_role']);
 $pageParams["nome"] = "../pages/viewNotification.php";
 require "../pages/base.php";
 ?>
