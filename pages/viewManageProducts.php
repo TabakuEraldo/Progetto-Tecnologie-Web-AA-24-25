272c824d6d15ce45a41629b269f97425d4abb7ef
patrick.sbrighi <patrick.sbrighi00@gmail.com>
2025-02-05 00:48:33 +0100

    Pagina modifica prodotto

--- pages/viewManageProducts.php
+++ pages/viewManageProducts.php
@@ -4,34 +4,36 @@
     <?php if (empty($listino)): ?>
         <div class="alert alert-info">Il listino è vuoto.</div>
     <?php else: ?>
-        <table class="table">
-            <thead class="table-light table-hover table-bordered">
-                <tr>
-                    <th>Immagine</th>
-                    <th>Nome</th>
-                    <th>Prezzo</th>
-                    <th>Categoria</th>
-                    <th>Descrizione</th>
-                    <th>Quantità</th>
-                    <th>Azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($listino as $item):?>
+        <div class="table-responsive">
+            <table class="table">
+                <thead class="table-light table-hover table-bordered">
                     <tr>
-                        <td>
-                            <img src="../img/<?php echo htmlspecialchars($item['immagine']); ?>" class="img-fluid" alt="Immagine prodotto">
-                        </td>
-                        <td><?php echo htmlspecialchars($item['nome']); ?></td>
-                        <td><?php echo number_format($item['prezzo'], 2, ',', '.'); ?>€</td>
-                        <td><?php echo htmlspecialchars($item['categoria']); ?></td>
-                        <td><?php echo htmlspecialchars($item['descrizione']); ?></td>
-                        <td><?php echo htmlspecialchars($item['disponibilita']); ?></td>
-                        <td class="text-end"><a class="btn btn-primary btn-sm" href="../php/modificaProdotto.php?id=<?php echo $item['id']; ?>">Modifica</a></td>
+                        <th>Immagine</th>
+                        <th>Nome</th>
+                        <th>Prezzo</th>
+                        <th>Categoria</th>
+                        <th>Descrizione</th>
+                        <th>Quantità</th>
+                        <th>Azioni</th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach($listino as $item):?>
+                        <tr>
+                            <td>
+                                <img src="../img/<?php echo htmlspecialchars($item['immagine']); ?>" class="img-fluid" style="max-width: 100px; height: auto;" alt="Immagine prodotto">
+                            </td>
+                            <td><?php echo htmlspecialchars($item['nome']); ?></td>
+                            <td><?php echo number_format($item['prezzo'], 2, ',', '.'); ?>€</td>
+                            <td><?php echo htmlspecialchars($item['categoria']); ?></td>
+                            <td><?php echo htmlspecialchars($item['descrizione']); ?></td>
+                            <td><?php echo htmlspecialchars($item['disponibilita']); ?></td>
+                            <td class="text-end"><a class="btn btn-primary btn-sm" href="../php/modificaProdotto.php?id=<?php echo $item['id']; ?>">Modifica</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     <?php endif; ?>
 </div>
 
--- pages/viewModifyProduct.php
+++ pages/viewModifyProduct.php
@@ -1,1 +1,39 @@
-<h1>Modificami tutto</h1>
+<?php if(empty($pageParams["modificaProd"])):?>
+    <p class="text-muted text-center w-100">Nessun prodotto selezionato</p>
+    <a href="../php/manageProducts.php" class="btn btn-secondary btn-sm">Indietro</a>
+<?php else: $prod= $pageParams["modificaProd"];?>
+    <div class="mt-3">
+        <h2>Modifica prodotto</h2>
+        <form action="../php/executeModificaProdotto.php" method="POST" enctype="multipart/form-data" class="row g-3">
+            <div class="col-md-6">
+                <label for="nome" class="form-label fw-semibold fs-6 text-dark">Nome</label>
+                <input type="text" class="form-control" id="nome" name="nome" value=<?php echo $prod["nome"];?> required>
+            </div>
+            <div class="col-md-6">
+                <label for="prezzo" class="form-label fw-semibold fs-6 text-dark">Prezzo (€)</label>
+                <input type="number" step="0.01" class="form-control" id="prezzo" name="prezzo" value=<?php echo $prod["prezzo"];?> required>
+            </div>
+            <div class="col-md-6">
+                <label for="categoria" class="form-label fw-semibold fs-6 text-dark">Categoria</label>
+                <input type="text" class="form-control" id="categoria" name="categoria" value=<?php echo $prod["categoria"];?> required>
+            </div>
+            <div class="col-md-6">
+                <label for="disponibilita" class="form-label fw-semibold fs-6 text-dark">Quantità</label>
+                <input type="number" class="form-control" id="disponibilita" name="disponibilita" value=<?php echo $prod["disponibilita"];?> required>
+            </div>
+            <div class="col-12">
+                <label for="descrizione" class="form-label fw-semibold fs-6 text-dark">Descrizione</label>
+                <textarea class="form-control" id="descrizione" name="descrizione" rows="3" required><?php echo $prod["descrizione"];?> </textarea>
+            </div>
+            <div class="col-12">
+                <label for="immagine" class="form-label fw-semibold fs-6 text-dark">Immagine (non è ncessario se non vuoi cambiare immagine)</label>
+                <input type="hidden" name="immagine_attuale" value="<?php echo $prod["immagine"]; ?>">
+                <input type="file" class="form-control" id="immagine" name="immagine" accept="image/*">
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Modifica Prodotto</button>
+            </div>
+        </form>
+    </div>
+<?php endif;?>
+
